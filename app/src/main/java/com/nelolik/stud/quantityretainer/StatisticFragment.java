d0130edf9a6c2149629cda6076f31b3cd023ed33
nelolik <parshikovav@gmail.com>
2019-10-13 20:39:28 +0300

    Сделано отображение в чате по дням и по месяцам.

--- app/src/main/java/com/nelolik/stud/quantityretainer/StatisticFragment.java
+++ app/src/main/java/com/nelolik/stud/quantityretainer/StatisticFragment.java
@@ -11,6 +11,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.XAxis;
@@ -37,7 +38,8 @@ public class StatisticFragment extends Fragment {
     private Handler mDbThreadHandler;
     private BarChart mChart;
     private ArrayList<BarEntry> mPointsList;
-    private ArrayList<String> mLabels = new ArrayList<>();
+    private RadioGroup mIntervalRadioGroup;
+    private boolean intervalDay, intervalMonth;
 
 
     public StatisticFragment() {
@@ -56,6 +58,8 @@ public class StatisticFragment extends Fragment {
         mWorkingThread = new HandlerThread("RetentionThread");
         mWorkingThread.start();
         mDbThreadHandler = new Handler(mWorkingThread.getLooper());
+        intervalDay = true;
+        intervalMonth = false;
 
     }
 
@@ -64,9 +68,43 @@ public class StatisticFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_statistic, container, false);
+        mIntervalRadioGroup = view.findViewById(R.id.statistic_interval);
+        mIntervalRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.statistic_days) {
+                    intervalDay = true;
+                    intervalMonth = false;
+                }
+                if (checkedId == R.id.statistic_months) {
+                    intervalDay = false;
+                    intervalMonth = true;
+                }
+                if (mRetProvider != null) {
+                    mDbThreadHandler.post(() -> {
+                        getDataToDisplay();
+                        BarData data = prepareBarData();
+
+                        Activity activity = getActivity();
+                        if (activity != null && data != null) {
+                            activity.runOnUiThread(() -> {
+                                mChart.setData(data);
+                                mChart.moveViewToX(mChart.getXChartMax());
+                                mChart.setVisibleXRangeMaximum(7);
+                                mChart.setVisibleXRangeMinimum(7);
+                                mChart.animateY(100);
+                                mChart.invalidate();
+
+                            });
+                        }
+                    });
+                }
+            }
+        });
         mChart = view.findViewById(R.id.chart);
         mChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-        mChart.getXAxis().setDrawGridLines(false);
+        mChart.getXAxis().setDrawGridLines(true);
+        mChart.getXAxis().setGranularity(1f);
         mChart.getXAxis().setTextSize(16.0f);
         mChart.getXAxis().setLabelRotationAngle(90);
         mChart.getXAxis().setValueFormatter(new ValueFormatter() {
@@ -74,8 +112,22 @@ public class StatisticFragment extends Fragment {
 
             @Override
             public String getFormattedValue(float value) {
-                long timestamp = (long)(value * 1000 * 86400) + 43200000;   //43200000 - middle of a day
-                return formater.format(timestamp);
+                long t = 0;
+                if (intervalDay) {
+                    formater.applyPattern("dd MMMM");
+                    t = (long)value * 1000 * 86400;
+                }
+                if (intervalMonth) {
+                    formater.applyPattern("LLLL YYYY");
+                    int year = ((int)value) / 12;
+                    int month = ((int)value) % 12;
+                    Calendar c = Calendar.getInstance();
+                    c.set(year, month,1);
+                    t = c.getTimeInMillis();
+                }
+
+                String lbl = formater.format(t);
+                return lbl;
             }
         });
         mChart.getAxisLeft().setAxisMinimum(0);
@@ -94,31 +146,16 @@ public class StatisticFragment extends Fragment {
         if (mRetProvider != null) {
             mDbThreadHandler.post(() -> {
                 getDataToDisplay();
-                BarDataSet dataSet = new BarDataSet(mPointsList, "Retention");
-                dataSet.setColor(Color.BLUE);
-//                dataSet.setFormSize(30);
-                dataSet.setValueTextSize(16);
-                dataSet.setBarBorderColor(Color.BLUE);
-                dataSet.setBarBorderWidth(3);
-                BarData data = new BarData(dataSet);
-                data.setBarWidth(0.8f);
-                data.setValueFormatter(new ValueFormatter() {
-                    @Override
-                    public String getFormattedValue(float value) {
-                        if (value > 0) {
-                            return String.valueOf((int)value);
-                        }
-                        return "";
-                    }
-                });
+                BarData data = prepareBarData();
 
                 Activity activity = getActivity();
-                if (activity != null && dataSet != null) {
+                if (activity != null && data != null) {
                     activity.runOnUiThread(() -> {
                         mChart.setData(data);
-                        float lastIndex = mPointsList.get(0).getX();
-                        mChart.moveViewToX(mChart.getXChartMax() - 7);
+                        mChart.moveViewToX(mChart.getXChartMax());
                         mChart.setVisibleXRangeMaximum(7);
+                        mChart.setVisibleXRangeMinimum(7);
+//                        mChart.animateY(100);
                         mChart.invalidate();
 
                     });
@@ -144,28 +181,58 @@ public class StatisticFragment extends Fragment {
             return new ArrayList<BarEntry>();
         }
         HashMap<Long, Integer> dataMap = new HashMap<Long, Integer>();
-        Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("");
         int columnCountIndex = cursor.getColumnIndex(RetainDBContract.RetainEntity.COLUMN_COUNT);
         int columnDateIndex = cursor.getColumnIndex(RetainDBContract.RetainEntity.COLUMN_DATE);
         int count;// = cursor.getInt(columnCountIndex);
         long timestamp;// = cursor.getLong(columnDateIndex);
         cursor.moveToPosition(-1);
+        Calendar calendar = Calendar.getInstance();
         while (cursor.moveToNext()) {
             count = cursor.getInt(columnCountIndex);
             timestamp = cursor.getLong(columnDateIndex);
-            if (timestamp < 100000) {continue;}
-            long day = timestamp / 1000 / 86400;    //86400 - seconds in day
-            if (dataMap.containsKey(day)) {
-                dataMap.put(day, count + dataMap.get(day));
+            if (timestamp < 100000) {continue;} //HZ why
+            long key;
+            if (intervalDay) {
+                key = timestamp / 1000 / 86400;    //86400 - seconds in day
             } else {
-                dataMap.put(day, count);
+                calendar.setTimeInMillis(timestamp);
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                key = year * 12 + month;
+            }
+
+            if (dataMap.containsKey(key)) {
+                dataMap.put(key, count + dataMap.get(key));
+            } else {
+                dataMap.put(key, count);
             }
         }
 
         mPointsList = mapToSortedList(dataMap);
         addDataIfListShort();
         return mPointsList;
+    }
+
+    private BarData prepareBarData() {
+        BarDataSet dataSet = new BarDataSet(mPointsList, "Retention");
+        dataSet.setColor(Color.BLUE);
+//                dataSet.setFormSize(30);
+        dataSet.setValueTextSize(16);
+        dataSet.setBarBorderColor(Color.BLUE);
+        dataSet.setBarBorderWidth(3);
+        BarData data = new BarData(dataSet);
+        data.setBarWidth(0.8f);
+        data.setValueFormatter(new ValueFormatter() {
+            @Override
+            public String getFormattedValue(float value) {
+                if (value > 0) {
+                    return String.valueOf((int)value);
+                }
+                return "";
+            }
+        });
+        return data;
     }
 
     ArrayList<BarEntry> mapToSortedList(HashMap<Long, Integer> data) {
@@ -180,7 +247,6 @@ public class StatisticFragment extends Fragment {
             sortedKeys.add(position, key);
         }
         ArrayList<BarEntry> sortedData = new ArrayList<>();
-        int i = 0;
         for (Long k : sortedKeys) {
             sortedData.add(new BarEntry(k, data.get(k)));
         }
@@ -188,12 +254,11 @@ public class StatisticFragment extends Fragment {
     }
 
     private void addDataIfListShort() {
-        if (mPointsList.size() > 0 && mPointsList.size() < 17) {
-            BarEntry be = mPointsList.get(mPointsList.size() - 1);
-            long day = (long)be.getX();
-            while (mPointsList.size() < 17) {
-                day -= 1;
-                mPointsList.add(new BarEntry(day, 0));
+        if (mPointsList.size() > 0 && mPointsList.size() < 8) {
+            BarEntry be = mPointsList.get(0);
+            long x = (long)be.getX();
+            while (mPointsList.size() < 8) {
+                mPointsList.add(new BarEntry(--x, 0));
             }
         }
     }
